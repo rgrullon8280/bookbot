46f0a632e53a0f92c18b3f5390dff1099c416ace
Ricky Grullon <rgrullon@figma.com>
2025-03-30 12:53:43 -0400

    print a report

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from stats import get_num_words, get_character_count
+from stats import (
+    get_num_words, 
+    get_character_count,
+    get_sorted_char_counts
+)
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,14 +10,24 @@ def get_book_text(filepath):
 
     return contents
 
+def print_a_report(book_path, word_count, character_counts):
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {book_path}...')
+    print('----------- Word Count ----------')
+    print(f"Found {word_count} total words")
+    print('--------- Character Count -------')
+    for dict in character_counts:
+        print(f"{dict['character']}: {dict['count']}")
+    print('============= END ===============')
+
+
 def main():
-    book_path = './books/frankenstein.txt'
+    book_path = 'books/frankenstein.txt'
     book_contents = get_book_text(book_path)
     num_words = get_num_words(book_contents)
     character_counts = get_character_count(book_contents)
-    print(f"{num_words} words found in the document")
-    for key,value in character_counts.items():
-        print(f"'{key}': {value}")
+    sorted_character_counts = get_sorted_char_counts(character_counts)
+    print_a_report(book_path, num_words, sorted_character_counts)
 
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,19 @@ def get_character_count(string):
         else:
             count_dict[char_lower] = 1
     return count_dict
+
+def get_sorted_char_counts(char_dict):
+    # pythonic way
+    # char_dict_list = [{'character': key, 'count': value} for key, value in char_dict.items() if key.isalpha()]
+    # Normal way
+    char_dict_list = []
+    for char, count in char_dict.items():
+        if char.isalpha():
+            char_dict_list.append(
+                {
+                    'character': char,
+                    'count': count
+                }
+            )
+    sorted_char_dict_list = sorted(char_dict_list, key=lambda x: x['count'], reverse=True)
+    return(sorted_char_dict_list)
