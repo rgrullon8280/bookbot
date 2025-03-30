0b2c46ffd902a40285407c2c9dbe543c210ec00e
Ricky Grullon <rgrullon@figma.com>
2025-03-30 11:56:59 -0400

    create get_book_contents function

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('greetings boots')
+def get_book_text(filepath):
+    with open(filepath) as f:
+        contents = f.read()
+
+    return contents
+
+
+def main():
+    book_contents = get_book_text('./books/frankenstein.txt')
+    print(book_contents)
+
+
+main()
